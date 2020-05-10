125962f01f0ed1bf0b8418e34618c57e76fd3976
Aimen Sahnoun <62159014+aimensahnoun@users.noreply.github.com>
2020-05-10 21:30:40 +0300

    Update Service_Announcer.py

--- Service_Announcer.py
+++ Service_Announcer.py
@@ -10,26 +10,28 @@ IP = ip.split('.')
 IP[3] = '255'
 ip = '.'.join(IP)
 
-files = []
+
 
 
 def getFiles(*args):
     for file in os.listdir(os.curdir):
-        if '.' not in file:
+        if os.path.isdir(os.path.join(os.curdir, file)):
+            for subFile in os.listdir(os.path.join(os.curdir, file)):
+                if '.' not in subFile and subFile not in files:
+                    files.append(subFile)
+        if '.' not in file and file not in files:
             files.append(file)
 
-
-getFiles(files)
-
-t = {'username': username, 'files': files}
-
-message = json.dumps(t)
 
 service = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP);
 
 service.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
 while True:
-    service.sendto(bytes(message, "utf-8"), (ip, 5000))
+    files = []
+    getFiles(files)
+    t = {'username': username, 'files': files}
+    message = json.dumps(t)
+    service.sendto(bytes(message, "utf-8"), (socket.gethostbyname(socket.gethostbyname()), 5000))
     print("message sent!")
     time.sleep(60)
